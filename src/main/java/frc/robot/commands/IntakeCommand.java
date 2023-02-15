1f2a2a4bec94be291bc39906d24b1fcf47d6fd03
Programmers3539 <bbprogrammers3539@gmail.com>
2023-02-15 18:40:54 -0500

    Update IntakeCommand.java

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -27,7 +27,10 @@ public class IntakeCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    RobotContainer.intakeSubsystem.setIntakeSpeed(0);
+
+  }
 
   // Returns true when the command should end.
   @Override
